4c517cd46304f183462d93d856f6417a47d88bc9
pcwd <41215821+pcwd@users.noreply.github.com>
2024-09-11 13:48:06 +1000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -13,7 +13,7 @@ export default {
     "LNURL1DP68GURN8GHJ7MRWVF5HGUEWD3H8WCTVD3JHGTNPWPCZ7MRWW4EXCUP0FEXXW6JCD5PE9Y7N",
 
   // name of your MongoDB database
-  dbName: "user",
+  dbName: "users",
 
   // name of your MongoDB collection
   dbCollection: "registered",
